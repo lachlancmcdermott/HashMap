d631091dda0d2d0d87304857f947ee302ee84185
Lachlan McDermott <lachlancmcd@outlook.com>
2023-04-05 17:01:17 -0700

    Add project files.

---
+++ HashMap/HashMap.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMap
+{
+    public class HashMap<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        public List<(TKey, TValue)> hashList = new List<(TKey, TValue)>();
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException();
+
+        public ICollection<TValue> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        IEqualityComparer<TKey> equalityComparer => default;
+
+        public void Add(TKey key, TValue value)
+        {
+            int hash = equalityComparer.GetHashCode(key);
+            if(hash > hashList.Capacity)
+            {
+                //increase capacity of list, check that it has enough capacity
+                //rehash data in previous list, add all data to new list
+                //replace and set new list to previous list
+                hashList.Capacity
+            }
+            hashList.Insert(hash, (key, value));
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HashMap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HashMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashMap<string, int> hashMap = new HashMap<string, int>();
+            hashMap.Add("cat", 1);
+            hashMap.Add("dog", 2);
+            hashMap.Add("frog", 3);
+
+            
+        }
+    }
+}
