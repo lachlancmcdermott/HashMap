264af5b5e41ba093433e696985296ddae7e9c416
Lachlan McDermott <lachlancmcd@outlook.com>
2023-04-08 12:00:55 -0700

    most of hashmap implemeted, need to finish indexing get{}, set{} properties.

--- HashMap/HashMap.cs
+++ HashMap/HashMap.cs
@@ -1,98 +1,128 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace HashMap
 {
     public class HashMap<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        public List<(TKey, TValue)>[] hashList = new List<(TKey, TValue)>[5];
+        public LinkedList<(TKey, TValue)>[] hashList = new LinkedList<(TKey, TValue)>[3];
+        private int keyCount = 0;
+
         public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
         public ICollection<TKey> Keys => throw new NotImplementedException();
-
         public ICollection<TValue> Values => throw new NotImplementedException();
-
-        public int Count;
-
         public bool IsReadOnly => throw new NotImplementedException();
-
         IEqualityComparer<TKey> equalityComparer => EqualityComparer<TKey>.Default;
+
+        int ICollection<KeyValuePair<TKey, TValue>>.Count => throw new NotImplementedException();
 
         public void Add(TKey key, TValue value)
         {
             int hash = Math.Abs(equalityComparer.GetHashCode(key)) % hashList.Length;
-            Count++; 
-            
-            if(hashList.Count >= hashList.Length)
+            keyCount++;
+
+            if(keyCount > hashList.Length)
             {
-                //increase capacity of list, check that it has enough capacity
-                //rehash data in previous list, add all data to new list
-                //replace and set new list to previous list
-
-                List<(TKey, TValue)> newHashList = new List<(TKey, TValue)>(hashList.Length * 2);
-                for (int i = 0; i < hashList.Count; i++)
+                //resize list   
+                LinkedList<(TKey, TValue)>[] newList = new LinkedList<(TKey, TValue)>[hashList.Length * 2];
+                for (int i = 0; i < hashList.Length; i++)
                 {
-                    int temp = Math.Abs(equalityComparer.GetHashCode(hashList[i].Item1)) % newHashList.Capacity;
-                    newHashList.Insert(temp, hashList[i]);
+                    if (hashList[i] != null)
+                    {
+                        foreach (var item in hashList[i])
+                        {
+                            int tempHash = Math.Abs(equalityComparer.GetHashCode(hashList[i].Last.Value.Item1)) % hashList.Length;
+                            if (newList[tempHash] == null)
+                            {
+                                newList[tempHash] = new LinkedList<(TKey, TValue)>();
+                                newList[tempHash].AddLast(new LinkedListNode<(TKey, TValue)>((item.Item1, item.Item2)));
+                            }
+                            else
+                            {
+                                newList[tempHash].AddLast(new LinkedListNode<(TKey, TValue)>((item.Item1, item.Item2)));
+                            }
+                        }
+                        hashList[i].RemoveLast();
+                    }
                 }
-                hashList = newHashList;
+                hashList = newList;
             }
-            hashList.Insert(hash, (key, value));
-        }
+            if (hashList[hash] != null && hashList[hash].First != null)
+            {
+                LinkedListNode<(TKey, TValue)> temp = hashList[hash].Find((key, value));
+                if (temp == null)
+                {
+                    hashList[hash].AddLast(new LinkedListNode<(TKey, TValue)>((key, value)));
+                }
+                else
+                {
+                    Exception bad = new Exception("Key/Value combo already exists");
+                    throw bad;
+                }
+            }
+            if (hashList[hash] == null)
+            {
+                hashList[hash] = new LinkedList<(TKey, TValue)>();
+                hashList[hash].AddLast(new LinkedListNode<(TKey, TValue)>((key, value)));
 
-        public void Add(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+            }
         }
-
-        public void Clear()
+        public bool Contains(KeyValuePair<TKey, TValue> Item)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ContainsKey(TKey key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            for (int i = 0; i < hashList.Length; i++)
+            {
+                if (hashList[i] != null)
+                {
+                    foreach (var item in hashList[i])
+                    {
+                        return item.Item1.Equals(Item.Key) && item.Item2.Equals(Item.Value);
+                    }
+                }
+            }
+            return false;
         }
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            int hash = Math.Abs(equalityComparer.GetHashCode(key)) % hashList.Length;
+            if (hashList[hash] != null)
+            {
+                foreach (var item in hashList[hash])
+                {
+                    if(item.Item1.Equals(key))
+                    {
+                        hashList[hash].Remove(item);
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
-        public bool Remove(KeyValuePair<TKey, TValue> item)
+        public bool Indexing(TKey key)
         {
-            throw new NotImplementedException();
+            throw new Exception("Key does not exist");
+
         }
 
-        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        //public int Count()
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < hashList.Length; i++)
+        //    {
+        //        for (int k = 0; k < hashList[i].Count; k++)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
     }
 }
--- HashMap/Program.cs
+++ HashMap/Program.cs
@@ -7,9 +7,7 @@ namespace HashMap
         static void Main(string[] args)
         {
             HashMap<string, int> hashMap = new HashMap<string, int>();
-            hashMap.Add("cat", 1);
-            hashMap.Add("dog", 2);
-            hashMap.Add("frog", 3);
+
         }
     }
 }
